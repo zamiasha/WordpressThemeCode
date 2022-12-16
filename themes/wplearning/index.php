a2e74f6cd14a16771bf47672d44ded5778c4e2ad
zamia s <sherzaman@geniusmindzone.com>
2022-12-16 17:19:38 +0500

    remove  title(<h1><?php the_title() ?></h1>)

--- themes/wplearning/index.php
+++ themes/wplearning/index.php
@@ -13,7 +13,7 @@ get_header();
       
       <?php  if ( have_posts() ) : ?>
            <?php  while ( have_posts() ) : the_post(); ?>   
-               <h1><?php the_title() ?></h1>
+          
           <?php  endwhile; ?>
       <?php  endif;  ?>
       </div>
