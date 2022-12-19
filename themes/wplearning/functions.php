32a068be053d636940559e3ea35f6dd9ef94cd77
zamia s <sherzaman@geniusmindzone.com>
2022-12-19 10:25:36 +0500

    simple jquery tag add

--- themes/wplearning/functions.php
+++ themes/wplearning/functions.php
@@ -7,6 +7,7 @@ function wplearning_theme_scripts() {
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assests/bootstrap/css/bootstrap.min.css');
     # wp_enqueue_style('style', get_stylesheet_directory_uri()); me  code
+    wp_enqueue_script('jquery')
     wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/assests/bootstrap/js/bootstrap.min.js'); 
 } 
 
