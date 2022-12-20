98c8561c83fb4904badeab9287703a554e4b0ec8
zamia s <sherzaman@geniusmindzone.com>
2022-12-20 12:26:30 +0500

    header  file   added theme structur for home page with classes

--- themes/wplearning/index.php
+++ themes/wplearning/index.php
@@ -12,7 +12,7 @@ get_header();
         <?php  if ( have_posts() ) : ?>
            <?php  while ( have_posts() ) : the_post(); ?>   
                <h1><?php the_title() ?></h1>
-               <p> <?php the_content() ?></p>
+               <p> <?php the_content() ?></p> 
           <?php  endwhile; ?>
       <?php  endif;  ?>
      </div>
