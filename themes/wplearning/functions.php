1d1494a80b84513c5bf8c7a3185bc4a7d09acaed
zamia s <sherzaman@geniusmindzone.com>
2022-12-20 12:48:21 +0500

    added  function file  time zone for last  change in css file

--- themes/wplearning/functions.php
+++ themes/wplearning/functions.php
@@ -4,7 +4,7 @@
  */
 
 function wplearning_theme_scripts() {
-    wp_enqueue_style('style', get_template_directory_uri() . '/style.css', the_time());
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css', the_time());    
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assests/bootstrap/css/bootstrap.min.css');
     # wp_enqueue_style('style', get_stylesheet_directory_uri()); me  code
     wp_enqueue_script('jquery');
