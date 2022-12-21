aba9a19a22dc608dffe4ec295725f9e39173254f
zamia s <sherzaman@geniusmindzone.com>
2022-12-21 10:28:12 +0500

    menu display  function added

--- themes/wplearning/functions.php
+++ themes/wplearning/functions.php
@@ -6,6 +6,10 @@
 
 
  add_theme_support('custom-logo');
+ register_nav_menus( array(
+    'primary'   => __( 'Primary Menu', 'myfirsttheme' ),
+    'secondary' => __( 'Secondary Menu', 'myfirsttheme' )
+) );
 
 function wplearning_theme_scripts() {
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css', the_time());    
