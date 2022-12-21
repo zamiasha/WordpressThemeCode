a475af035cecedbe663c2dd3806314da49927c76
zamia s <sherzaman@geniusmindzone.com>
2022-12-21 14:54:38 +0500

    display side  bar in frontend  added code and  added 1  css class for  design

--- themes/wplearning/sidebar.php
+++ themes/wplearning/sidebar.php
@@ -4,9 +4,9 @@
  */
 ?> 
 
-         <div class="">
+         <div class="main-sidebar-inner">
             <?php 
-                dynamic_sidebar( 'main-sidebar' );
+                dynamic_sidebar('main-sidebar');
 
                 ?>
          </div>
