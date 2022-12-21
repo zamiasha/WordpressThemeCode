77ef4116e22c6f51eb618948629d5c7ae23cfef5
zamia s <sherzaman@geniusmindzone.com>
2022-12-21 13:12:16 +0500

    post tumbnail( like, images )  support function

--- themes/wplearning/functions.php
+++ themes/wplearning/functions.php
@@ -11,11 +11,13 @@
             
             add_theme_support('title-tag');
 
+            add_theme_support('post-thumbnails');
+
             register_nav_menus( array(
             'primary'   => __( 'Primary Menu', 'wplearning' ),
             ) );
             };
-        add_action('after_setup_theme', 'wplearning_theme_setup')
+        add_action('after_setup_theme', 'wplearning_theme_setup');
 
 
 
