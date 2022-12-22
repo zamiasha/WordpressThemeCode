eea4eaf3b34d616a14f7417a775280c0b2ff2722
zamia s <sherzaman@geniusmindzone.com>
2022-12-22 15:32:14 +0500

    remove html old code  added  new  in to index file

--- themes/wplearning/index.php
+++ themes/wplearning/index.php
@@ -12,8 +12,8 @@ get_header();
         <div class="home-posts col-8 ">
             <?php  if ( have_posts() ) : ?>
             <?php  while ( have_posts() ) : the_post(); ?>   
-                <h1><?php the_title() ?></h1>
-                <p> <?php the_content() ?></p> 
+                
+            
             <?php  endwhile; ?>
         <?php  endif;  ?>
         </div>
