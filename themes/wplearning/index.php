7863be7aac9efe949da8ff79cdf73a05e0a26f61
zamia s <sherzaman@geniusmindzone.com>
2022-12-22 15:59:53 +0500

    post header

--- themes/wplearning/index.php
+++ themes/wplearning/index.php
@@ -12,8 +12,17 @@ get_header();
         <div class="home-posts col-8 ">
             <?php  if ( have_posts() ) : ?>
             <?php  while ( have_posts() ) : the_post(); ?>   
-                
-            
+                         
+               <article class="home-post">
+                  <div class="post-header">
+                    <div class="post-thumbnail  row  ml-0 mr-0">
+                        <?php the_post_thumbnail('full') ?>
+                    </div>
+                  </div>
+                  <div class="post-description"></div>
+                  <div class="post-footer"></div>
+               </article>
+                   
             <?php  endwhile; ?>
         <?php  endif;  ?>
         </div>
