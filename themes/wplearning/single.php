bb3820f319c7869376c81f08af1b4f7e57f42b36
zamia s <sherzaman@geniusmindzone.com>
2022-12-29 00:44:53 +0500

    added post data code  display in single post

--- themes/wplearning/single.php
+++ themes/wplearning/single.php
@@ -9,7 +9,12 @@ get_header();
 ?>
       
       
-      <h1>Hellow folks! I am learning Theme developement</h1>
+      <?php  if ( have_posts() ) : ?>
+           <?php  while ( have_posts() ) : the_post(); ?>   
+               <h1><?php the_title() ?></h1>
+               <p> <?php the_content() ?></p>
+          <?php  endwhile; ?>
+      <?php  endif;  ?>
 
 <?php
    get_footer();
