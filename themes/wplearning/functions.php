b4d5cd7b80d7e4795b5b33273f544168c5f4d7f5
zamia s <sherzaman@geniusmindzone.com>
2022-12-29 14:59:49 +0500

    added custom header-img added  defult  img, widt, height, color, and responsive  and  create  1  arys to assign value

--- themes/wplearning/functions.php
+++ themes/wplearning/functions.php
@@ -22,6 +22,16 @@
             register_nav_menus( array(
             'primary'   => __( 'Primary Menu', 'wplearning' ),
             ) );
+             
+            $args = array(
+                'deafult-image' => get_template_directory_uri().'/assests/img/header-1.jpg',
+                'default-text-color'  =>  '000',
+                'width'         => 1920,
+                'height'        => 400,
+                'flex-width'    => true,
+                'flex-height'   => true,
+            );
+            add_theme_support( "custom-header", $args  ); 
             };
         add_action('after_setup_theme', 'wplearning_theme_setup');
 
