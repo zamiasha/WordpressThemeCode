37df157376473d7c07c02802398b74c09c1a296c
zamia s <sherzaman@geniusmindzone.com>
2022-12-30 12:47:09 +0500

    defined  single  post  layout from backend and  defined   classes

--- themes/wplearning/single.php
+++ themes/wplearning/single.php
@@ -8,13 +8,31 @@
 get_header();
 ?>
       
-      
+       <div class="post-content">
+        
       <?php  if ( have_posts() ) : ?>
            <?php  while ( have_posts() ) : the_post(); ?>   
-               <h1><?php the_title() ?></h1>
-               <p> <?php the_content() ?></p>
+            <div class="post-image">
+                <?php the_post_thumbnail('home-featured')  ?>
+            </div>
+            <div class="post-title">
+                <h1> <?php the_title() ?></h1>
+            </div>
+                <div class="post-meta">
+                    <div class="post-meta">
+                        <strong> Author: <?php the_author() ?> </strong>
+                    </div>
+                    <div class="post-meta">
+                        <strong> Posdted on : <?php the_time() ?> </strong>
+                    </div>
+                </div>
+                <div class="post-content">
+                 <p>  <?php the_content() ?> </p>
+                </div>      
+               
           <?php  endwhile; ?>
       <?php  endif;  ?>
+      </div>
 
 <?php
    get_footer();
